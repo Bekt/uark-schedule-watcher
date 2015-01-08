087afcfd6313703829087ffa79f73e9e08ee8bd5
Bekt <bekt17@gmail.com>
2015-01-07 22:16:57 -0600

    Hooked up frontend to backend.

--- src/handlers.py
+++ src/handlers.py
@@ -10,20 +10,24 @@ EMAIL_RE = re.compile(r'[^@]+@[^@]+\.[^@]+')
 class AppHandler(webapp2.RequestHandler):
 
     def watch(self):
-        email = self.request.get('email')
-        class_num = self.request.get('course')
+        req = json.loads(self.request.body)
+        email = str(req.get('email', ''))
+        class_num = str(req.get('course', ''))
         term = 1153  # lol
+        # term = req.get('term')
         if (EMAIL_RE.match(email) is None or 
                 not email.endswith('uark.edu')):
             logging.error('Email invalid: {}'.format(email))
             self.write_json({'message': 'Invalid @uark email.'}, 400)
             return
-        if not class_num.isdigit():
+        if (not class_num.isdigit()
+                or int(class_num) < 1 or int(class_num) > 100000):
             logging.error('Course number invalid: {}'.format(class_num))
             self.write_json({'message': 'Invalid course number.'}, 400)
             return
-        #watcher = Watcher(email=email, class_num=class_num, strm=term)
-        #watcher.put()
+        class_num = int(class_num)
+        watcher = Watcher(email=email.lower(), class_num=class_num, strm=term)
+        watcher.put()
         self.write_json({})
 
     def write_json(self, response, status=200):
