8037bc20eefd61b9f862b3d7f65055dfdda4f85b
Bekt <bekt17@gmail.com>
2015-03-08 14:47:25 -0500

    May, Summer, August, Fall semesters list.

--- src/handlers.py
+++ src/handlers.py
@@ -6,6 +6,14 @@ from models import Watcher
 
 EMAIL_RE = re.compile(r'[^@]+@[^@]+\.[^@]+')
 
+active_terms = [{'code': t[0], 'name': t[1]}
+                for t in [
+                    (1154, 'May Intersession 2015'),
+                    (1156, 'Summer 2015'),
+                    (1157, 'August Intersession 2015'),
+                    (1159, 'Fall 2015'),
+                    ]]
+
 
 class AppHandler(webapp2.RequestHandler):
 
@@ -13,24 +21,31 @@ class AppHandler(webapp2.RequestHandler):
         req = json.loads(self.request.body)
         email = str(req.get('email', ''))
         class_num = str(req.get('course', ''))
-        term = 1153  # lol
-        # term = req.get('term')
+        term = str(req.get('term', ''))
         if (EMAIL_RE.match(email) is None or
                 not email.endswith('uark.edu')):
-            logging.error('Email invalid: {}'.format(email))
+            logging.warning('Email invalid: {}'.format(email))
             self.write_json({'message': 'Invalid @uark email.'}, 400)
             return
         if (not class_num.isdigit()
                 or int(class_num) < 1 or int(class_num) > 100000):
-            logging.error('Course number invalid: {}'.format(class_num))
+            logging.warning('Course number invalid: {}'.format(class_num))
             self.write_json({'message': 'Invalid course number.'}, 400)
             return
-        class_num = int(class_num)
-        watcher = Watcher(email=email.lower(), class_num=class_num, strm=term)
+        if (not term.isdigit()
+                or not any(t['code'] == int(term) for t in active_terms)):
+            logging.warning('Term code not invalid: {}'.format(term))
+            self.write_json({'message': 'Invalid term number.'}, 400)
+            return
+        watcher = Watcher(email=email.lower(),
+                          class_num=int(class_num), strm=int(term))
         watcher.put()
-        self.write_json({})
+        self.write_json()
 
-    def write_json(self, response, status=200):
+    def terms(self):
+        self.write_json(active_terms)
+
+    def write_json(self, response={}, status=200):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.set_status(status)
         self.response.out.write(json.dumps(response))
@@ -43,4 +58,6 @@ app = webapp2.WSGIApplication([
                   handler='cron.CronHandler:process'),
     webapp2.Route(r'/watch', methods=['POST'],
                   handler='handlers.AppHandler:watch'),
+    webapp2.Route(r'/terms', methods=['GET'],
+                  handler='handlers.AppHandler:terms'),
 ])
